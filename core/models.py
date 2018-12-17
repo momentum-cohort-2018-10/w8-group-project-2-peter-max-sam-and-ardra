2098347394d91c32196146c0f9f09faf56f42f7f
iceviper1914 <mehiorob@gmail.com>
2018-12-17 17:58:07 -0500

    working basic models setup

--- core/models.py
+++ core/models.py
@@ -1,6 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+
+class Quiz(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="quizzes")
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=255)
+    date_created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 
-class User(AbstractUser):
-    
+class Card(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="cards")
+    question = models.TextField(blank=False, null=False)
+    answer = models.TextField(blank=False, null=False)
+    right_answers = models.IntegerField(blank=False)
+    wrong_answers = models.IntegerField(blank=False)
