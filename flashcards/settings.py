7ed55f66b6b1bef04c15be1610626d2bd7930ef0
Sam LoCascio <sflocascio@gmail.com>
2018-12-20 18:28:48 -0500

    necessary changes for heroku deploy

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +133,5 @@ EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
 EMAIL_PORT = 1025
 LOGIN_REDIRECT_URL = "home"
+
+django_heroku.settings(locals())
---
+++ procfile
@@ -0,0 +1,1 @@
+web: gunicorn flashcards.wsgi
